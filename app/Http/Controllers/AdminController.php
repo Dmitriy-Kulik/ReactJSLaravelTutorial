7c3d23b9fb4f7e782ef14170a0174d66117b6fdd
Dmitriy-Kulik <0663642996d@gmail.com>
2018-03-31 00:46:54 +0300

    first commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        $users = DB::table('users')->get();
+        return view('admin', compact('users'));
+    }
+
+    public function getUser($id){
+
+    }
+
+    public function addUser(){
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware' => 'auth'], function(){
+    //тут роуты для которых нужна авторизация
+    Route::get('/home/protected/user', 'UserController@index');
+
+    Route::group(['middleware' => 'admin', 'auth'], function(){
+        Lang::setlocale('ru');
+        $l = Lang::getlocale();
+        echo $l;
+        //тут роуты только для админа + авторизация
+        Route::get('/home/protected/admin', 'AdminController@index');
+    });
+});
+
+
+
+
+Route::get(
+    '/socialite/{provider}',
+    [
+        'as' => 'socialite.auth',
+        function ( $provider ) {
+            return \Socialite::driver( $provider )->redirect();
+        }
+    ]
+);
+
+Route::get( '/socialite/{provider}/callback', [
+    function ( $provider ) {
+        $user = \Socialite::driver( $provider )->user();
+        dd( $user );
+    }
+] );
